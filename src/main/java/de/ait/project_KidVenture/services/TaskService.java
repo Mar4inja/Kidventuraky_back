abe6fbe780e4b939a387818c63862377555a54a5
Martins Groza <mar4inja19@gmail.com>
2024-05-30 22:36:37 +0200

    tasks

--- src/main/java/de/ait/project_KidVenture/services/TaskService.java
+++ src/main/java/de/ait/project_KidVenture/services/TaskService.java
@@ -1,4 +1,24 @@
 package de.ait.project_KidVenture.services;
 
+import de.ait.project_KidVenture.entity.Task;
+
+import java.util.List;
+
 public interface TaskService {
+
+    List<Task> getAllTasks();
+
+    Task getTaskById(Long id);
+
+    Task createTask(Task task);
+
+    Task updateTask(Task task);
+
+    void deleteTask(Long id);
+
+    List<Task> searchTaskByDifficulty(String difficulty);
+
+    List<Task> searchByType(String taskType);
+
+
 }
